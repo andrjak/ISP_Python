b2e5094d2c3c2f0be1b95ba4aee1c987714bd3c6
andrjak <79821843rt@gmail.com>
2019-05-17 23:27:02 +0300

    min fix

--- Model/DBcontroler.py
+++ Model/DBcontroler.py
@@ -16,8 +16,14 @@ def get_data(this_company):
         date = datetime.datetime.strptime(i["date"], "%d.%m.%Y")
         worker_list.append(Worker(i["name"], i["position"], birthday, i["salary"],
                                   i["experience"], i["duration"], date))
-    this_company.start_init([Transport(TransportInfo("S8", "Audi", 2015))],
-                            worker_list, [1, 2, 3], [1, 2, 3], {"Программист": 2})
+
+    transports = db["transport_list"].find()
+    transport_list = list()
+    for i in transports:
+        transport_list.append(Transport(TransportInfo(i["name"], i["producer"], i["year"]),
+                                        i["insurance"], i["warranty_service"]))
+
+    this_company.start_init(transport_list, worker_list, [1, 2, 3], [1, 2, 3], {"Программист": 2})
 
 
 def set_data(this_company):
@@ -27,29 +33,18 @@ def set_data(this_company):
         workers.save({"name": i.name, "position": i.position, "birthday": i.birthday.strftime("%d.%m.%Y"),
                       "salary": i.salary, "experience": i.experience,
                       "duration": i.duration, "date": i.date.strftime("%d.%m.%Y")})
+    transports = db["transport_list"]
+    transports.remove({})
+    for i in this_company.transportList:
+        transports.save({"name": i.info.name, "producer": i.info.producer, "year": i.info.year,
+                         "insurance": i.insurance, "warranty_service": i.warranty_service})
 
 
 def main():
-    this_company = company.Company()
-    workers = [Worker("Валетко Андрей Николаевич", "Директор", datetime.date(2000, 4, 21),
-                      1000, 2, 8, datetime.date(2018, 2, 1)),
-               Worker("Горбачёв Дмитрий", "Зам Директора", datetime.date(2000, 8, 4),
-                      900, 2, 8, datetime.date(2018, 2, 1)),
-               Worker("Матюшонак Александр", "Зам Директора", datetime.date(2000, 1, 19),
-                      950, 2, 8, datetime.date(2018, 2, 1))]
-
-    this_company.start_init([1, 2, 3], workers, [1, 2, 3], [1, 2, 3], {"Программист": 2})
-
-    worker_collection = db["worker_list"]
-    worker_collection.save({"name": workers[0].name, "position": workers[0].position, "birthday": "21.04.2000",
-                            "salary": workers[0].salary, "experience": workers[0].experience,
-                            "duration": workers[0].duration, "date": "01.02.2018"})
-    worker_collection.save({"name": workers[1].name, "position": workers[1].position, "birthday": "04.08.2000",
-                            "salary": workers[1].salary, "experience": workers[1].experience,
-                            "duration": workers[1].duration, "date": "01.02.2018"})
-    worker_collection.save({"name": workers[2].name, "position": workers[2].position, "birthday": "19.01.2000",
-                            "salary": workers[2].salary, "experience": workers[2].experience,
-                            "duration": workers[2].duration, "date": "01.02.2018"})
+    transport_collection = db["transport_list"]
+    tr = Transport(TransportInfo("S8", "Audi", 2015))
+    transport_collection.save({"name": tr.info.name, "producer": tr.info.producer, "year": tr.info.year,
+                               "insurance": tr.insurance, "warranty_service": tr.warranty_service})
 
 
 if __name__ == "__main__":
